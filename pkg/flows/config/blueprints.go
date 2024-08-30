86863d7ee5dbae1d9c2c010fd5c780f705c75178
Thorsten Klein <tk@thklein.io>
2024-08-30 15:33:24 +0200

    add: blueprint for new context tool

--- pkg/flows/config/blueprints.go
+++ pkg/flows/config/blueprints.go
@@ -8,8 +8,12 @@ import (
 //go:embed blueprints/default.yaml
 var BlueprintDefault []byte
 
+//go:embed blueprints/context.yaml
+var BlueprintContext []byte
+
 var Blueprints = map[string][]byte{
 	"default": BlueprintDefault,
+	"context": BlueprintContext,
 }
 
 func GetBlueprint(name string) ([]byte, error) {
